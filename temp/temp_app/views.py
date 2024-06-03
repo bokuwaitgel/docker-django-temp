f278249e02ea33724e0601895c2945edb6a92ff5
itgl <itgel6708@gmail.com>
2024-06-03 16:26:06 +0800

    temp

---
+++ temp/temp_app/views.py
@@ -0,0 +1,16 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from temp_app.models import Todo
+
+class TodoView(APIView):
+    
+    def post (self, request):
+        task = request.data.get('task')
+        completed = request.data.get('completed')
+        todo = Todo.objects.create(task=task, completed=completed)
+        return Response(200)
+    
+    def get(self, request):
+        todos = Todo.objects.all()
+        return Response([{'task': todo.task, 'completed': todo.completed} for todo in todos])
